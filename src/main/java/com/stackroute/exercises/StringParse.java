e1159278d385a544ecc0146a3df332ac5802ee4f
shahid <shahdbest@gmail.com>
2022-08-17 15:16:17 +0530

    a

---
+++ src/main/java/com/stackroute/exercises/StringParse.java
@@ -0,0 +1,9 @@
+package com.stackroute.exercises;
+
+
+public class StringParse {
+    //Write logic to find the character from the sentence at specified location
+    public char characterLocator(String sentence, String location) {
+        return 0;
+    }
+}
